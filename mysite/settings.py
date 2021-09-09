56f1971b82569441a9c5a0aac78985db8ea02b31
yukichi <ykt.0412@gmail.com>
2021-09-09 12:59:32 +0900

    Tutorial Finished

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'djangogirls',
         'USER': 'nishida',
         'PASSWORD': '1995westfield',
@@ -129,3 +129,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGIN_REDIRECT_URL = '/'
